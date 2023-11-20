8025d582738c3cb8857fa165363d84a7fdb0eb63
Jens Utbult <jens.utbult@yubico.com>
2023-11-20 18:26:35 +0100

    Fixed broken SPM file and changed required versions for iOS to 16.0 and macOS to 13.

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "YubiKitSwift",
+    name: "YubiKit",
     platforms: [
-            .macOS(.v13), .iOS(.v16)
-        ],
+        .macOS(.v13), .iOS(.v16)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "YubiKitSwift",
-            targets: ["YubiKitSwift"]),
+            name: "YubiKit",
+            targets: ["YubiKit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,11 +22,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "YubiKitSwift",
+            name: "YubiKit",
             path: "YubiKit/YubiKit"
+        ),
         .testTarget(
-            name: "YubiKitSwiftTests",
-            dependencies: ["YubiKitSwift"],
+            name: "YubiKitTests",
+            dependencies: ["YubiKit"],
             path: "YubiKit/UnitTests"
         ),
     ]
