36a0d729ed1b1f5ea9a581a5d79345e5d5c9d93d
Jens Utbult <jens.utbult@yubico.com>
2024-01-25 17:07:17 +0100

    Dependencies are now working for GzipSwift.

--- Package.swift
+++ Package.swift
@@ -17,13 +17,19 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/1024jp/GzipSwift", from: "6.0.1"),
+        
+        // The name of the GzipSwift repository is different from the package
+        // name which confuses SPM. This is a kludge until we either solve
+        // the issue or simply copy the source code from GzipSwift to our
+        // repo instead.
+        .package(url: "https://github.com/jensutbult/Gzip", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "YubiKit",
+            dependencies: ["Gzip"],
             path: "YubiKit/YubiKit"
         ),
         .testTarget(
