434173f21b474d4a5d2fde866cd9174519b7fcb7
João D. Moreira <joao.moreira@yubico.com>
2025-04-14 16:34:00 +0200

    Move the gzip helper as a 3P file inside our sdk

--- Package.swift
+++ Package.swift
@@ -9,27 +9,13 @@ let package = Package(
         .macOS(.v13), .iOS(.v16)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "YubiKit",
             targets: ["YubiKit"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        
-        // The name of the GzipSwift repository is different from the package
-        // name which confuses SPM. This is a kludge until we either solve
-        // the issue or simply copy the source code from GzipSwift to our
-        // repo instead.
-        .package(url: "https://github.com/jensutbult/Gzip", branch: "main"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "YubiKit",
-            dependencies: ["Gzip"],
             path: "YubiKit/YubiKit"
         ),
         .testTarget(
